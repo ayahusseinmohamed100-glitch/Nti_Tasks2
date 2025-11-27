76ce4700bba348f2720411a8b51f81c361783b4f
Aya Hussein <ayahusseinmohamed100@gmail.com>
2025-11-27 16:06:39 +0200

    Update nti_task_Last2digit.dart

---
+++ nti_task_Last2digit.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  int n1 = int.parse(stdin.readLineSync()!);
+  int n2 = int.parse(stdin.readLineSync()!);
+  int n3 = int.parse(stdin.readLineSync()!);
+  int n4 = int.parse(stdin.readLineSync()!);
+  int m = n1 * n2 * n3 * n4;
+  print(m % 100);
+}
